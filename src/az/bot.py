c1adb6325210caf1e4c58a100c5677afe3996603
Björn Petersen <git@bjoernpetersen.net>
2023-07-10 09:40:30 +0200

    Adjust prompts

--- src/az/bot.py
+++ src/az/bot.py
@@ -5,7 +5,6 @@ from typing import Any, TypedDict
 import aiohttp
 import openai
 from telegram import Update
-from telegram.error import TimedOut
 from telegram.ext import Application, CommandHandler
 
 from az.config import Config
@@ -21,12 +20,16 @@ class Message(TypedDict):
     content: str
 
 
-_PROMPT = """
-Schlage einen Vodka-Cocktail mit zwei Zutaten vor, benenne ihn nach dem Schema Vodka-<name>.
+_SYSTEM_PROMPT = """
+Du bist ein Assistent, der Nutzern bei der Getränkeauswahl hilft, indem du einen
+außergewöhnlichen Drink vorschlägst. Das Getränk sollte immer ein Longdrink
+auf Vodkabasis mit zwei Zutaten sein. Benenne den Drink nach dem Schema
+"Vodka-<Buchstabe>", also Vodka-E, Vodka-A, Vodka-T, etc.
+
 Bevorzuge ungewöhnliche Zutaten! Solange es essbar bzw. trinkbar ist, ist es ein guter
 Vorschlag!
 
-Eine Liste von Beispielen:
+Gute Beispiele:
 - Vodka-O: Vodka mit Orangensaft
 - Vodka-E: Vodka mit Eistee
 - Vodka-G: Vodka mit Gurkenwasser
@@ -53,6 +56,10 @@ Eine Liste von Beispielen:
 - Vodka-B: Vodka mit Blaubeerlikör
 - Vodka-J: Vodka mit Johannisbeersaft
 - Vodka-H: Vodka mit Honig
+"""
+
+_USER_PROMPT = """
+Schlag mir einen Drink vor, und fass dich kurz.
 """
 
 _LOG = logging.getLogger(__name__)
@@ -91,14 +98,17 @@ class AzBot:
 
     async def _suggest_drink(self) -> str:
         response = await openai.ChatCompletion.acreate(  # type: ignore
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             max_tokens=75,
             messages=[
-                Message(role=Role.USER, content=_PROMPT),
+                Message(role=Role.SYSTEM, content=_SYSTEM_PROMPT),
+                Message(role=Role.USER, content=_USER_PROMPT),
             ],
         )
 
-        return response.choices[0].message.content
+        result = response.choices[0].message.content
+        _LOG.debug("Received suggestion: %s", result)
+        return result
 
     async def _create_image(self, drink: str) -> bytes:
         response = await openai.Image.acreate(  # type: ignore
